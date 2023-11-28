312c19de8bf1d6bf569057d26446c69729184b33
frank shu <frankshu@frankdeMac-Studio.local>
2023-11-28 15:34:58 +0800

    fix

--- page_crawler.py
+++ page_crawler.py
@@ -79,7 +79,9 @@ class PageCrawler(ABC):
                         try: 
                             wait = WebDriverWait(self._driver, 10)
                             wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, command['selector'])))
+                            print(f'element found {command["selector"]}')
                             self.driver.find_element(By.CSS_SELECTOR, command['selector']).click()
+                            print(f'element click {command["selector"]}')
                         except TimeoutException as ex:
                             print(f'click {command["selector"]} timeout')
 
